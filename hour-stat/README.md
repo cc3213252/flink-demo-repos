c58323f2414149a9138986dedaf5dee7bc2d8a9a
yudan.chen <cc3213252@163.com>
2022-05-31 20:53:47 +0800

    增加hour-stat

---
+++ hour-stat/README.md
@@ -0,0 +1,71 @@
+## 参考资料
+
+https://nightlies.apache.org/flink/flink-docs-release-1.15/zh/docs/try-flink/table_api/
+
+## 例子说明
+
+SpendReport 最简单的Table api实现的，从kafka到mysql，按小时统计每个账户的累计金额  
+SpendReportUdf 增加了自定义UDF，时间按小时取整  
+SpendReportWindow 增加了滚动窗口  
+
+## 依赖
+
+flink-table-api-java
+flink-table-api-java-bridge
+flink-table-planner_${scala.binary.version}
+flink-connector-kafka  
+flink-csv
+
+## 环境搭建（单机环境）
+
+### mysql
+
+```sql
+CREATE TABLE spend_report(
+    account_id BIGINT,
+    log_ts     TIMESTAMP(3),
+    amount     BIGINT,
+    PRIMARY KEY (account_id, log_ts)
+);
+```
+
+### kafka
+
+启动kafka：  
+bin/zookeeper-server-start.sh config/zookeeper.properties  
+bin/kafka-server-start.sh config/server.properties  
+
+创建topic:  
+bin/kafka-topics.sh --create --topic transactions --bootstrap-server localhost:9092  
+
+查看topic:
+bin/kafka-topics.sh --bootstrap-server localhost:9092 --list
+
+删除topic:
+bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic transactions
+
+发送数据：  
+bin/kafka-console-producer.sh --topic transactions --bootstrap-server localhost:9092  
+1,100,2022-05-16 09:00:00
+2,200,2022-05-16 09:10:00
+1,100,2022-05-16 09:05:00
+3,100,2022-05-16 09:05:00
+
+bin/kafka-console-consumer.sh --topic transactions --from-beginning --bootstrap-server localhost:9092
+
+
+## 问题
+
+运行测试用例报：Failed to get job status so we assume that the job has terminated  
+
+SpendReportWindow测试没效果
+
+## 官网例子
+
+apache/data-generator 这个镜像用例生成数据  
+
+docker中jobmanager无法启动
+
+## 注意
+
+mac系统升级，导致mysql无法登陆，重新安装解决
---
+++ hour-stat/src/main/java/com/blueegg/SpendReportWindow.java
@@ -0,0 +1,58 @@
+package com.blueegg;
+
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.Tumble;
+import static org.apache.flink.table.api.Expressions.$;
+import static org.apache.flink.table.api.Expressions.lit;
+
+
+public class SpendReportWindow {
+
+    public static Table report(Table transactions) {
+        return transactions
+                .window(Tumble.over(lit(1).hour()).on($("transaction_time")).as("log_ts"))
+                .groupBy($("account_id"), $("log_ts"))
+                .select(
+                        $("account_id"),
+                        $("log_ts").start().as("log_ts"),
+                        $("amount").sum().as("amount"));
+    }
+
+    public static void main(String[] args) throws Exception {
+        EnvironmentSettings settings = EnvironmentSettings.newInstance().build();
+        TableEnvironment tEnv = TableEnvironment.create(settings);
+
+        tEnv.executeSql("CREATE TABLE transactions (\n" +
+                "    account_id  BIGINT,\n" +
+                "    amount      BIGINT,\n" +
+                "    transaction_time TIMESTAMP(3),\n" +
+                "    WATERMARK FOR transaction_time AS transaction_time - INTERVAL '5' SECOND\n" +
+                ") WITH (\n" +
+                "    'connector' = 'kafka',\n" +
+                "    'topic'     = 'transactions',\n" +
+                "    'properties.group.id' = 'testGroup',\n" +
+                "    'properties.bootstrap.servers' = 'localhost:9092',\n" +
+                "    'scan.startup.mode' = 'earliest-offset',\n" +
+                "    'format'    = 'csv'\n" +
+                ")");
+
+        tEnv.executeSql("CREATE TABLE spend_report (\n" +
+                "    account_id BIGINT,\n" +
+                "    log_ts     TIMESTAMP(3),\n" +
+                "    amount     BIGINT\n," +
+                "    PRIMARY KEY (account_id, log_ts) NOT ENFORCED" +
+                ") WITH (\n" +
+                "  'connector'  = 'jdbc',\n" +
+                "  'url'        = 'jdbc:mysql://localhost:3306/sink_test?useUnicode=true&characterEncoding=utf-8',\n" +
+                "  'table-name' = 'spend_report',\n" +
+//                "  'driver'     = 'com.mysql.cj.jdbc.Driver',\n" +
+                "  'username'   = 'root',\n" +
+                "  'password'   = ''\n" +
+                ")");
+
+        Table transactions = tEnv.from("transactions");
+        report(transactions).executeInsert("spend_report");
+    }
+}
